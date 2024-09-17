76292ae32087315edb679f27b275194c041dc39f
AlcinoMachavane <150256503+AlcinoMachavane@users.noreply.github.com>
2024-09-17 08:03:08 +0100

    Novos ajustes

--- src/components/inputs/multItemsInput/multItemsInput.js
+++ src/components/inputs/multItemsInput/multItemsInput.js
@@ -5,7 +5,6 @@ const contactsUl = document.getElementById('contactsUl');
 
 // Função para adicionar contato à lista
 function addContact(event) {
-    // Evita o comportamento padrão do botão, que é recarregar a página
     if (event) {
         event.preventDefault();
     }
@@ -14,13 +13,30 @@ function addContact(event) {
 
     // Verifica se o campo de entrada não está vazio
     if (contactValue) {
+        // Cria o elemento <li> para o contato
         const li = document.createElement('li');
-        li.textContent = contactValue;
+
+        // Adiciona o valor do contato dentro de um <span>
+        const span = document.createElement('span');
+        span.textContent = contactValue;
+        li.appendChild(span);
+
+        // Cria o botão de excluir
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '<i class="fa-regular fa-trash-can"></i>'; // Usa innerHTML para adicionar o ícone corretamente
+        deleteButton.classList.add('remove-button');
+        deleteButton.classList.add('iconButton');
+        li.appendChild(deleteButton);
+
+        // Adiciona o evento de clique para remover o contato
+        deleteButton.addEventListener('click', function () {
+            contactsUl.removeChild(li);
+        });
 
         // Adiciona o novo contato à lista
         contactsUl.appendChild(li);
 
-        // Limpa o campo de entrada após adicionar o contato
+        // Limpa o campo de entrada
         contactInput.value = '';
     } else {
         alert('Por favor, insira um número de contato válido.');
@@ -28,7 +44,6 @@ function addContact(event) {
 }
 
 // Adiciona evento de clique ao botão "Adicionar"
-// Passa o evento para a função addContact
 addContactButton.addEventListener('click', addContact);
 
 // Adiciona evento de pressionar "Enter" no campo de entrada
@@ -37,4 +52,3 @@ contactInput.addEventListener('keypress', function (e) {
         addContact();
     }
 });
-
